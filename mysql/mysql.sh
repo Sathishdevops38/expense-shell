06679d2307ae03a6bb437f5106c5127b12598bd1
sathishdevops38 <sathishdevops38@gmail.com>
2026-01-04 08:49:09 +0530

    fux

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -2,8 +2,8 @@
 #install mysql-serer 8.0
 dnf install mysql-server -y
 #start service
-sudo systemctl start mysqld
-#enable mysql
 sudo systemctl enable mysqld
+#enable mysql
+sudo systemctl start mysqld
 #set password
 sudo mysql_secure_installation --set-root-pass ExpenseApp@1
